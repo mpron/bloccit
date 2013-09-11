747f128cbbb4aa7bf3beba5f3a26850c999fc6ba
Mitch Pronschinske <mpronschinske@gmail.com>
2013-09-11 19:49:03 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bloccit
+
+A Rails-based Reddit clone application to demonstrate foundation programming and web development skills.
+
+Under construction
+
+[Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = '794f17d96c9a297af912b141badd5463fed65b41c7ee6e749e563cdbca673eb8d53604560a89e8dc9d0543d3208924ae98309dad8e85beaad2c6f88436f0c748'
