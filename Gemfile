a91a2b67eda87a39eee202a02fd92287e2216efd
Mitch Pronschinske <mpronschinske@gmail.com>
2013-09-22 11:19:19 -0400

    Installed and configured pagination

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'carrierwave'
 gem 'mini_magick', '~> 3.5.0'
 gem 'fog'
 gem 'figaro'
+gem 'will_paginate', '~> 3.0'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar
   has_many :posts
   before_create :set_member
-  mount_uploader :avatar, AvatarUploader # add this line.
+  
+  mount_uploader :avatar, AvatarUploader 
 
 end
