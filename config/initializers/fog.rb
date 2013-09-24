2c040f6d8e31a4ff4e695e58477c38cadb6893dc
Mitch Pronschinske <mpronschinske@gmail.com>
2013-09-23 20:17:03 -0400

    modified application.yml

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'], 
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], 
   } 
-  config.fog_directory  = 'redditability-development'
+  config.fog_directory  = ENV['AWS_BUCKET']
   config.fog_public     = false 
 end 
 
