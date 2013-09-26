ee2a6c18a9acc007dcb1ca0c57ea51f0738fb907
Mitch Pronschinske <mpronschinske@gmail.com>
2013-09-25 20:14:01 -0400

    Added Comments and comment editing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-
   def create
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
@@ -7,21 +6,14 @@ class CommentsController < ApplicationController
 
     @comment = current_user.comments.build(params[:comment])
     @comment.post = @post
-    @new_comment = Comment.new
-    
-    authorize! :create, @comment, message: "You need be signed in to do that."
 
+    authorize! :create, @comment, message: "You need be signed in to do that."
     if @comment.save
       flash[:notice] = "Comment was created."
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
+      render 'posts/show'
     end
-
-    respond_with(@comment) do |f|
-      f.html { redirect_to [@topic, @post] }
-    end
-  end
-
-  def show
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
   attr_accessible :body, :post
+
+
+  validates :body, length: { minimum: 2 }, presence: true
+  validates :user, presence: true
 end
