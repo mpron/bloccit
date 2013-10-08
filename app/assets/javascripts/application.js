38ba4965f7fd4af63cd780b6cc519bd4629b7c87
Mitch Pronschinske <mpronschinske@gmail.com>
2013-10-07 21:22:36 -0400

    jQuery additions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+$(document).ready(function() {
+  $(".js-add-comment").click(function() {
+    $(".js-new-comment").slideToggle();
+    return false;
+  });
+});
