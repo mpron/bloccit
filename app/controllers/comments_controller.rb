9df5bbe4ae203dd1a1866c601925834a7757405b
Mitch Pronschinske <mpronschinske@gmail.com>
2013-10-10 20:38:02 -0400

    destroys comments with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,11 +12,14 @@ class CommentsController < ApplicationController
     authorize! :create, @comment, message: "You need be signed in to do that."
     if @comment.save
       flash[:notice] = "Comment was created."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      render 'posts/show'
     end
+
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post] }
+    end
+
   end
 
   def destroy
@@ -28,10 +31,8 @@ class CommentsController < ApplicationController
     
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
     end
 
     respond_with(@comment) do |f|
